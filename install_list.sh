0ffc9b6ca65c02eb5bf69f90f09b04c35817b08a
Chip Thien <themacrochip@gmail.com>
2020-06-07 15:04:38 -0500

    Update nvm version

--- install_list.sh
+++ install_list.sh
@@ -45,7 +45,7 @@ sudo apt-get install -y heroku
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 wget -qO- https://get.docker.com/ | sh
 
